c0b232ac7a7bf5ce5e8e6cc989566abdfb2c3583
Henry Ou <henryouly@gmail.com>
2015-03-03 15:18:40 -0800

    move config file to /etc

--- copy.sh
+++ copy.sh
@@ -4,3 +4,4 @@ cp /var/www/openmediavault/js/omv/module/admin/service/aria2/* var/www/openmedia
 cp /usr/share/openmediavault/engined/rpc/aria2.inc usr/share/openmediavault/engined/rpc/aria2.inc
 cp /usr/share/openmediavault/engined/module/aria2.inc usr/share/openmediavault/engined/module/aria2.inc
 cp /usr/share/openmediavault/mkconf/aria2 usr/share/openmediavault/mkconf/aria2
+cp /etc/init.d/aria2 etc/init.d/aria2
